7396aa53f6e423f6af16c286466257088f92bd9f
Stan Carney <stan.carney@rootsh.me>
2016-02-05 13:25:21 -0700

    It appears I misunderstood how IsValid() worked on reflect.

--- validators.go
+++ validators.go
@@ -5,9 +5,10 @@ import (
 	"reflect"
 )
 
+// IsNotZeroValueDeep is a function used by the validator.V2 GO lib. It should only be used on Structs for performance reasons. 
+// Builtins will perform much better using the 'nonzero' validator provided by the lib.
 func IsNotZeroValue(v interface{}, param string) error {
-	st := reflect.ValueOf(v)
-	if !st.IsValid() {
+	if reflect.DeepEqual(v, reflect.Zero(reflect.TypeOf(v)).Interface()) {
 		return errors.New(CANNOT_BE_ZERO_VALUE)
 	}
 	return nil
---
+++ validators_test.go
@@ -0,0 +1,79 @@
+package gowork
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestIsNotZeroValue_IntZero(t *testing.T) {
+
+	//setup
+	v := 0
+
+	//execute
+	r := IsNotZeroValue(v, "")
+
+	//verify
+	require.Error(t, r)
+}
+
+func TestIsNotZeroValue_IntNotZero(t *testing.T) {
+
+	//setup
+	v := 1
+
+	//execute
+	r := IsNotZeroValue(v, "")
+
+	//verify
+	require.Nil(t, r)
+}
+
+func TestIsNotZeroValue_MonetaryAmountZero(t *testing.T) {
+
+	//setup
+	v := MonetaryAmount{}
+
+	//execute
+	r := IsNotZeroValue(v, "")
+
+	//verify
+	require.Error(t, r)
+}
+
+func TestIsNotZeroValue_MonetaryAmountNotZero(t *testing.T) {
+
+	//setup
+	v := Zero // Zero as in $0.00 not as in doesn't exist.
+
+	//execute
+	r := IsNotZeroValue(v, "")
+
+	//verify
+	require.Nil(t, r)
+}
+
+func TestIsNotZeroValue_ErrorResponseZero(t *testing.T) {
+
+	//setup
+	v := ErrorResponse{}
+
+	//execute
+	r := IsNotZeroValue(v, "")
+
+	//verify
+	require.Error(t, r)
+}
+
+func TestIsNotZeroValue_ErrorResponsePartialZero(t *testing.T) {
+
+	//setup
+	v := ErrorResponse{}
+	v.Count = 1
+
+	//execute
+	r := IsNotZeroValue(v, "")
+
+	//verify
+	require.Nil(t, r)
+}
