fe27400b94188e8e8d1411715c4686330922452f
Stan Carney <stan.carney@rootsh.me>
2016-02-11 16:21:39 -0700

    Passed in times need to be converted to the servers local timezone.

--- http.go
+++ http.go
@@ -23,8 +23,8 @@ func GetDate(r *http.Request) time.Time {
 	vals := r.URL.Query()
 	date := vals.Get("date")
 
-	if dt, err := time.Parse("2006-01-02T15:04:05-07:00", date); err == nil {
-		return FloorDay(dt).Local() 
+	if dt, err := time.ParseInLocation("2006-01-02T15:04:05-07:00", date, time.Local); err == nil {
+		return FloorDay(dt)
 	}
 
 	//Default timeless dates to Local.
