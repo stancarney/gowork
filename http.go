e00cba919c52fa610b9ca4c6464b91a764f4c72b
Stan Carney <stan.carney@rootsh.me>
2016-02-11 21:19:56 -0700

    Reworked how TZ's are specified. It was kind of weird specifying UTC-7 and having the date roll back.

--- http.go
+++ http.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+	"log"
 )
 
 func GetLimit(r *http.Request) (limit int) {
@@ -24,12 +25,23 @@ func GetDate(r *http.Request) time.Time {
 	date := vals.Get("date")
 
 	if dt, err := time.Parse("2006-01-02T15:04:05-07:00", date); err == nil {
-		return FloorDay(dt.Local()) 
+		local := dt.Local()
+		
+		// If somebody requests a given date in their TZ we don't want to roll the date ahead of forward to align it with the system TZ.
+		if dt.Day() != local.Day() {
+			local = local.AddDate(0, 0, dt.Day() - local.Day())
+		}
+
+		return FloorDay(local)
+	} else {
+		log.Printf("Could not parse date %s (%s)\n", err, GetCurrentFunctionName(3))
 	}
 
-	//Default timeless dates to Local.
+	// Default timeless dates to Local.
 	if dt, err := time.ParseInLocation("2006-01-02", date, time.Local); err == nil {
 		return FloorDay(dt)
+	} else {
+		log.Printf("Could not parse date %s (%s)\n", err, GetCurrentFunctionName(3))
 	}
 
 	return FloorDay(CurrentTime())
