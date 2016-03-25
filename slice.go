e05aa933be9d7fbe88ec552842eda5b1ee4af312
Stan Carney <stan.carney@rootsh.me>
2016-03-25 17:21:56 -0600

    Added ChopSlice and ChopSortedSlice as a way to group like elements in a Slice.

--- slice.go
+++ slice.go
@@ -3,6 +3,7 @@ package gowork
 import (
 	"reflect"
 	"fmt"
+	"errors"
 )
 
 func SliceContains(slice interface{}, v interface{}) bool {
@@ -57,4 +58,75 @@ func DumpSlice(slice interface{}) {
 	default:
 		panic(fmt.Sprintf("not a slice, %s", t))
 	}
+}
+
+// ChopSlice returns a map of the input slice grouped by the specified field name.
+func ChopSlice(slice interface{}, field string) (interface{}, error) {
+	if slice == nil {
+		return nil, errors.New("Slice is nil")
+	}
+
+	switch reflect.TypeOf(slice).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(slice)
+		if s.Len() == 0 {
+			return nil, errors.New("Slice is empty")
+		}
+
+		first := s.Index(0)
+		m := reflect.MakeMap(reflect.MapOf(reflect.Indirect(first).FieldByName(field).Type(), reflect.SliceOf(first.Type())))
+
+		for i := 0; i < s.Len(); i++ {
+			key := reflect.Indirect(s.Index(i)).FieldByName(field)
+			value := m.MapIndex(key)
+			if !value.IsValid() {
+				value = reflect.MakeSlice(reflect.SliceOf(first.Type()), 0, 1) //TODO:Stan make a better guess of length
+			}
+			value = reflect.Append(value, s.Index(i))
+			m.SetMapIndex(key, value)
+		}
+
+		return m.Interface(), nil
+	default:
+		return nil, errors.New("Argument is not a slice")
+	}
+
+	return nil, errors.New("Something bad happened")
+}
+
+// ChopSortedSlice is similar to ChopSlice except rather than returning new slices for the map values it just re-slices the input slice, as a result the input slice must be sorted.
+// If the input slice is not sorted the results are undefined.
+func ChopSortedSlice(slice interface{}, field string) (interface{}, error) {
+
+	if slice == nil {
+		return nil, errors.New("Slice is nil")
+	}
+
+	switch reflect.TypeOf(slice).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(slice)
+		if s.Len() == 0 {
+			return nil, errors.New("Slice is empty")
+		}
+
+		first := s.Index(0)
+		m := reflect.MakeMap(reflect.MapOf(reflect.Indirect(first).FieldByName(field).Type(), reflect.SliceOf(first.Type())))
+
+		start := 0
+		for i := 0; i < s.Len(); i++ {
+			key := reflect.Indirect(s.Index(i)).FieldByName(field)
+			value := m.MapIndex(key)
+			if !value.IsValid() {
+				start = i
+			}
+			value = s.Slice(start, i + 1)
+			m.SetMapIndex(key, value)
+		}
+
+		return m.Interface(), nil
+	default:
+		return nil, errors.New("Argument is not a slice")
+	}
+
+	return nil, errors.New("Something bad happened")
 }
--- slice_test.go
+++ slice_test.go
@@ -103,3 +103,261 @@ func TestStringMapToSlice_Success(t *testing.T) {
 	require.Equal(t, true, sessions[0].Id == "1" || sessions[1].Id == "1")
 	require.Equal(t, true, sessions[0].Id == "2" || sessions[1].Id == "2")
 }
+
+func TestChopSlice_Valid(t *testing.T) {
+
+	//setup
+	args := []Session{
+		{Id: "1", UserId: "A"},
+		{Id: "2", UserId: "B"},
+		{Id: "3", UserId: "A"},
+		{Id: "4", UserId: "B"},
+		{Id: "5", UserId: "C"},
+	}
+
+	//execute
+	r, err := ChopSlice(args, "UserId")
+
+	//verify
+	require.NoError(t, err)
+
+	m := r.(map[string][]Session)
+	require.Len(t, m["A"], 2)
+	require.Equal(t, args[0], m["A"][0])
+	require.Equal(t, args[2], m["A"][1])
+
+	require.Len(t, m["B"], 2)
+	require.Equal(t, args[1], m["B"][0])
+	require.Equal(t, args[3], m["B"][1])
+
+	require.Len(t, m["C"], 1)
+	require.Equal(t, args[4], m["C"][0])
+}
+
+func TestChopSlice_ValidPointer(t *testing.T) {
+
+	//setup
+	args := []*Session{
+		{Id: "1", UserId: "A"},
+		{Id: "2", UserId: "B"},
+		{Id: "3", UserId: "A"},
+		{Id: "4", UserId: "B"},
+		{Id: "5", UserId: "C"},
+	}
+
+	//execute
+	r, err := ChopSlice(args, "UserId")
+
+	//verify
+	require.NoError(t, err)
+
+	m := r.(map[string][]*Session)
+	require.Len(t, m["A"], 2)
+	require.Equal(t, args[0], m["A"][0])
+	require.Equal(t, args[2], m["A"][1])
+
+	require.Len(t, m["B"], 2)
+	require.Equal(t, args[1], m["B"][0])
+	require.Equal(t, args[3], m["B"][1])
+
+	require.Len(t, m["C"], 1)
+	require.Equal(t, args[4], m["C"][0])
+}
+
+func TestChopSlice_NilSlice(t *testing.T) {
+
+	//setup
+
+	//execute
+	r, err := ChopSlice(nil, "UserId")
+
+	//verify
+	require.EqualError(t, err, "Slice is nil")
+	require.Nil(t, r)
+}
+
+func TestChopSlice_EmptySlice(t *testing.T) {
+
+	//setup
+	args := []Session{}
+
+	//execute
+	r, err := ChopSlice(args, "UserId")
+
+	//verify
+	require.EqualError(t, err, "Slice is empty")
+	require.Nil(t, r)
+}
+
+func TestChopSlice_NotSlice(t *testing.T) {
+
+	//setup
+	args := 0
+
+	//execute
+	r, err := ChopSlice(args, "UserId")
+
+	//verify
+	require.EqualError(t, err, "Argument is not a slice")
+	require.Nil(t, r)
+}
+
+func TestChopSortedSlice_Valid(t *testing.T) {
+
+	//setup
+	args := []Session{
+		{Id: "1", UserId: "A"},
+		{Id: "3", UserId: "A"},
+		{Id: "2", UserId: "B"},
+		{Id: "4", UserId: "B"},
+		{Id: "5", UserId: "C"},
+	}
+
+	//execute
+	r, err := ChopSortedSlice(args, "UserId")
+
+	//verify
+	require.NoError(t, err)
+
+	m := r.(map[string][]Session)
+	require.Len(t, m["A"], 2)
+	require.Equal(t, args[0], m["A"][0])
+	require.Equal(t, args[1], m["A"][1])
+
+	require.Len(t, m["B"], 2)
+	require.Equal(t, args[2], m["B"][0])
+	require.Equal(t, args[3], m["B"][1])
+
+	require.Len(t, m["C"], 1)
+	require.Equal(t, args[4], m["C"][0])
+}
+
+func TestChopSortedSlice_ValidPointer(t *testing.T) {
+
+	//setup
+	args := []*Session{
+		{Id: "1", UserId: "A"},
+		{Id: "3", UserId: "A"},
+		{Id: "2", UserId: "B"},
+		{Id: "4", UserId: "B"},
+		{Id: "5", UserId: "C"},
+	}
+
+	//execute
+	r, err := ChopSortedSlice(args, "UserId")
+
+	//verify
+	require.NoError(t, err)
+
+	m := r.(map[string][]*Session)
+	require.Len(t, m["A"], 2)
+	require.Equal(t, args[0], m["A"][0])
+	require.Equal(t, args[1], m["A"][1])
+
+	require.Len(t, m["B"], 2)
+	require.Equal(t, args[2], m["B"][0])
+	require.Equal(t, args[3], m["B"][1])
+
+	require.Len(t, m["C"], 1)
+	require.Equal(t, args[4], m["C"][0])
+}
+
+func TestChopSortedSlice_NilSlice(t *testing.T) {
+
+	//setup
+
+	//execute
+	r, err := ChopSortedSlice(nil, "UserId")
+
+	//verify
+	require.EqualError(t, err, "Slice is nil")
+	require.Nil(t, r)
+}
+
+func TestChopSortedSlice_EmptySlice(t *testing.T) {
+
+	//setup
+	args := []Session{}
+
+	//execute
+	r, err := ChopSortedSlice(args, "UserId")
+
+	//verify
+	require.EqualError(t, err, "Slice is empty")
+	require.Nil(t, r)
+}
+
+func TestChopSortedSlice_NotSlice(t *testing.T) {
+
+	//setup
+	args := 0
+
+	//execute
+	r, err := ChopSortedSlice(args, "UserId")
+
+	//verify
+	require.EqualError(t, err, "Argument is not a slice")
+	require.Nil(t, r)
+}
+
+func BenchmarkChopSlice_Every10(b *testing.B) {
+	testSet := buildBenchmarkSlice(10000, 10)
+	b.ResetTimer()
+	chopSliceBenchmark(testSet, b)
+}
+
+func BenchmarkChopSlice_Every100(b *testing.B) {
+	testSet := buildBenchmarkSlice(10000, 100)
+	b.ResetTimer()
+	chopSliceBenchmark(testSet, b)
+}
+
+func BenchmarkChopSlice_Every1000(b *testing.B) {
+	testSet := buildBenchmarkSlice(10000, 1000)
+	b.ResetTimer()
+	chopSliceBenchmark(testSet, b)
+}
+
+func BenchmarkChopSortedSlice_Every10(b *testing.B) {
+	testSet := buildBenchmarkSlice(10000, 10)
+	b.ResetTimer()
+	chopSortedSliceBenchmark(testSet, b)
+}
+
+func BenchmarkChopSortedSlice_Every100(b *testing.B) {
+	testSet := buildBenchmarkSlice(10000, 100)
+	b.ResetTimer()
+	chopSortedSliceBenchmark(testSet, b)
+}
+
+func BenchmarkChopSortedSlice_Every1000(b *testing.B) {
+	testSet := buildBenchmarkSlice(10000, 1000)
+	b.ResetTimer()
+	chopSortedSliceBenchmark(testSet, b)
+}
+
+func buildBenchmarkSlice(num, every int) interface{} {
+	var testSet []Session
+	for i := 0; i < num; i++ {
+		testSet = append(testSet, Session{Id: string(i), UserId: string(i / every)})
+	}
+	return testSet
+}
+
+func chopSliceBenchmark(testSet interface{}, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		_, err := ChopSlice(testSet, "UserId")
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func chopSortedSliceBenchmark(testSet interface{}, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		_, err := ChopSortedSlice(testSet, "UserId")
+		if err != nil {
+			panic(err)
+		}
+	}
+}
