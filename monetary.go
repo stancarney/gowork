ee185b98ad3fcf91ca608529dc0af1f5a1813a93
Stan Carney <stan.carney@rootsh.me>
2016-05-05 16:10:56 -0600

    Monetary amount now enforces currencies. Trying to perform operations with different currencies will result in a panic.

--- monetary.go
+++ monetary.go
@@ -8,7 +8,6 @@ import (
 )
 
 var decZero = inf.NewDec(0, 2)
-var maZero = MonetaryAmount{Dec: inf.NewDec(0, 2)}
 
 func Zero(currency currencies.Currency) MonetaryAmount {
 	return MonetaryAmount{Dec: decZero, Currency: currency}
@@ -20,7 +19,10 @@ type MonetaryAmount struct {
 	Currency currencies.Currency
 }
 
+// Add will add the provided MonetaryAmount to the current MonetaryAmount and return a new MonetaryAmount with the summed value.
+// It will panic if the currencies differ.
 func (m MonetaryAmount) Add(ma MonetaryAmount) MonetaryAmount {
+	checkCurrencyMatch(m, ma)
 	return MonetaryAmount{Dec: new(inf.Dec).Add(m.Dec, ma.Dec), Currency: m.Currency}
 }
 
@@ -36,19 +38,25 @@ func (m MonetaryAmount) Abs() MonetaryAmount {
 	return MonetaryAmount{Dec: new(inf.Dec).Abs(m.Dec), Currency: m.Currency}
 }
 
-func (m MonetaryAmount) Round(scale int) string { //TODO:Stan change to return MA?
+func (m MonetaryAmount) Round(scale int) string {
+	//TODO:Stan change to return MA?
 	return MonetaryAmount{Dec: new(inf.Dec).Round(m.Dec, inf.Scale(scale), inf.RoundHalfUp), Currency: m.Currency}.String()
 }
 
-func (m MonetaryAmount) AssumeScale(scale int) string {  //TODO:Stan change to return MA?
+func (m MonetaryAmount) AssumeScale(scale int) string {
+	//TODO:Stan change to return MA?
 	return strings.Replace(m.Round(scale), ".", "", 1)
 }
 
+// Cmp compares the provided MonetaryAmount to the current MonetaryAmount and return -1, 0, 1 based on the underlying inf.Dec.Cmp function. 
+// It will panic if the currencies differ.
 func (m MonetaryAmount) Cmp(ma MonetaryAmount) int {
+	checkCurrencyMatch(m, ma)
 	return m.Dec.Cmp(ma.Dec)
 }
 
 func (m MonetaryAmount) IsZero() bool {
+	var maZero = MonetaryAmount{Dec: inf.NewDec(0, 2), Currency: m.Currency}
 	return m.Cmp(maZero) == 0
 }
 
@@ -79,4 +87,10 @@ func MonetaryAmountFromStringPanic(value string, cur string) MonetaryAmount {
 		panic(err)
 	}
 	return m
+}
+
+func checkCurrencyMatch(m MonetaryAmount, ma MonetaryAmount) {
+	if m.Currency != ma.Currency {
+		panic(fmt.Sprintf("Currency mismatch: %s != %s", m.Currency, ma.Currency))
+	}
 }
